9ba329bc460feef9e8258831ee2ea507cc86870f
ruislan <z17520@126.com>
2020-06-25 10:35:51 +0800

    solve q1332

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+fn main() {
+    let s = "abbba";
+    let m = s.len() / 2;
+    println!("{:?}", &s[..=m]);
+    println!("{:?}", &s[m..]);
+    println!("{:?}", &s.chars().rev().collect::<String>()[..=m]);
+    println!("{:?} is palindrome", is_palindrome("abb"));
+}
+
+fn is_palindrome(s: &str) -> bool {
+    let (mut left, mut right) = (0, s.len() - 1);
+    while left <= right {
+        if s[left..=left] != s[right..=right] { return false; }
+        right -= 1;
+        left += 1;
+    }
+    true
+}
