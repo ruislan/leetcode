9d0524aa3e62a1a871499ff6d0371369eb0c0561
ruislan <z17520@126.com>
2020-07-13 13:35:37 +0800

    solved q1502

--- src/q1502.rs
+++ src/q1502.rs
@@ -4,6 +4,17 @@ impl Solution {
     pub fn can_make_arithmetic_progression(arr: Vec<i32>) -> bool {
         // 方法1
         // 排序arr，然后从1..arr.len()开始迭代，如果两个数之间的差值不相同，直接返回false，迭代完返回true
-        true
+        let mut arr = arr;
+        arr.sort_by(|a, b| b.cmp(a));
+        let q = arr[0] - arr[1];
+        arr.windows(2).all(|x| x[0] - x[1] == q)
     }
+}
+
+#[test]
+fn test() {
+    // assert_eq!(Solution::can_make_arithmetic_progression(vec![]), false);
+    assert_eq!(Solution::can_make_arithmetic_progression(vec![3, 5, 1]), true);
+    assert_eq!(Solution::can_make_arithmetic_progression(vec![1, 2, 4]), false);
+    assert_eq!(Solution::can_make_arithmetic_progression(vec![1, 3]), true);
 }
