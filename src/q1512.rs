4f424282172e326d1e739d5cbad89c462d52c593
ruislan <ruislan@github.com>
2020-07-15 08:20:21 +0800

    solved q1512

--- src/q1512.rs
+++ src/q1512.rs
@@ -8,6 +8,19 @@ impl Solution {
         // 以nums[i]为索引，将里面的值加到count上，count += arr[nums[i]]
         // 然后将arr[nums[i]]增加1
         // 一直到迭代结束，返回count即是好数对的结果
-        0
+        let (mut count, mut arr) = (0, vec![0; 101]);
+        nums.iter().for_each(|&x| {
+            count += arr[x as usize];
+            arr[x as usize] += 1;
+        });
+        count
     }
+}
+
+#[test]
+fn test() {
+    // assert_eq!(Solution::num_identical_pairs(vec![]), 0);
+    assert_eq!(Solution::num_identical_pairs(vec![1, 2, 3, 1, 1, 3]), 4);
+    assert_eq!(Solution::num_identical_pairs(vec![1, 1, 1, 1]), 6);
+    assert_eq!(Solution::num_identical_pairs(vec![1, 2, 3]), 0);
 }
