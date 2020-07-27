eea7367f848fe7c6938309313dccbbaf57a98b13
ruislan <ruislan@github.com>
2020-07-27 09:20:23 +0800

    solved q5456 & q5472

--- src/q5456.rs
+++ src/q5456.rs
@@ -4,6 +4,8 @@ impl Solution {
     pub fn count_odds(low: i32, high: i32) -> i32 {
         // 方法1 O(n)
         // 迭代low..=high，过滤 x & 1 == 1，count结果返回
+        // Passed 392ms 2.1mb
+        // (low..=high).filter(|&x| x & 1 == 1).count() as i32
 
         // 方法2 O(1)
         // 利用公式处理(high - low + high & 1 + low & 1) >> 1
@@ -14,6 +16,19 @@ impl Solution {
         // (4,7) = (7 - 4 + 1 + 0) / 2 = 2, (5,7)
         // (2,7) = (7 - 2 + 1 + 0) / 2 = 3, (3,5,7)
         // (3,8) = (8 - 3 + 0 + 1) / 2 = 3, (3,5,7)
-        0
+        // println!("{} - {} + {} + {} = {}", high, low, high & 1, low & 1, (high - low + (high & 1) + (low & 1)));
+        (high - low + (high & 1) + (low & 1)) >> 1
     }
+}
+
+#[test]
+fn test() {
+    assert_eq!(Solution::count_odds(0, 0), 0);
+    assert_eq!(Solution::count_odds(3, 7), 3);
+    assert_eq!(Solution::count_odds(1, 7), 4);
+    assert_eq!(Solution::count_odds(8, 10), 1);
+    assert_eq!(Solution::count_odds(4, 10), 3);
+    assert_eq!(Solution::count_odds(4, 7), 2);
+    assert_eq!(Solution::count_odds(2, 7), 3);
+    assert_eq!(Solution::count_odds(3, 8), 3);
 }
--- src/q5472.rs
+++ src/q5472.rs
@@ -3,8 +3,21 @@ use crate::Solution;
 impl Solution {
     pub fn restore_string(s: String, indices: Vec<i32>) -> String {
         // 方法1
-        // 创建res，迭代indices，按照indices[i]的索引从s中取出来逐个push到res中
-        // 返回res
-        "".to_string()
+        // 创建一个字符串数组arr，长度与indices一样，迭代indices，有索引i，对应&s[i]放到arr[indices[i]]中
+        // 返回arr组合成的字符串
+        // 注意:如果只是ascii的字符的话，&s[i..=i]的取值完全没问题，但是如果有Unicode字符，就必须用chars()来解决了
+        let mut arr = vec![' '; indices.len()];
+        indices.iter().zip(s.chars()).for_each(|(&x, ch)| arr[x as usize] = ch);
+        arr.into_iter().collect()
     }
+}
+
+#[test]
+fn test() {
+    assert_eq!(Solution::restore_string("".to_string(), vec![]), "".to_string());
+    assert_eq!(Solution::restore_string("codeleet".to_string(), vec![4, 5, 6, 7, 0, 2, 1, 3]), "leetcode".to_string());
+    assert_eq!(Solution::restore_string("abc".to_string(), vec![0, 1, 2]), "abc".to_string());
+    assert_eq!(Solution::restore_string("aiohn".to_string(), vec![3, 1, 4, 2, 0]), "nihao".to_string());
+    assert_eq!(Solution::restore_string("aaiougrt".to_string(), vec![4, 0, 2, 6, 7, 3, 1, 5]), "arigatou".to_string());
+    assert_eq!(Solution::restore_string("art".to_string(), vec![1, 0, 2]), "rat".to_string());
 }
