1e78adbf37b1de1109f14b8dd37f51639e8e4826
ruislan <ruislan@github.com>
2020-08-13 17:30:20 +0800

    solved offer24; add simple quicksort algorithm to sorter.rs; add idea to interview0104;

--- src/offer/offer24.rs
+++ src/offer/offer24.rs
@@ -19,13 +19,49 @@ impl ListNode {
 impl Solution {
     pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         // 方法1
-        // 迭代链表，将所有的值取出来放入stack
-        // 然后pop所有的值，用node包裹，next指向前一个节点
+        // 迭代链表，将所有的值取出来放入数组，顺序迭代数组，将值包装成节点，同时将该节点的下一个节点指向之前的那个节点
+        // 0ms 2.5mb
+        // let mut arr = Vec::new();
+        // let mut node = head.as_ref();
+        // while let Some(cur) = node {
+        //     arr.push(cur.val);
+        //     node = cur.next.as_ref();
+        // }
+        // let mut head = None;
+        // arr.into_iter().for_each(|x| {
+        //     head = Some(Box::new(ListNode { val: x, next: head.take() }));
+        // });
+        // head
 
         // 方法2
-        // 创建临时的指针prev表示上一个节点，初始为None
-        // 逐步访问链表，将当前节点的next指向prev
-        // 然后将当前节点作为前一个节点继续迭代
-        head
+        // 方法1改良一下，不需要创建数组，直接在迭代时候创建出新的链表，更简洁，但是效率上还是创建了新的空间
+        // 0ms 2.6mb
+        // let mut head = head.as_ref();
+        // let mut node = None;
+        // while let Some(cur) = head {
+        //     node = Some(Box::new(ListNode { val: cur.val, next: node.take() }));
+        //     head = cur.next.as_ref();
+        // }
+        // node
+
+        // 方法3
+        // 改良方法2，在原链表上进行操作，不用新的
+        // Passed 0ms 2.3mb
+        let mut head = head;
+        let mut node = None;
+        while head.is_some() {
+            let mut next = head.as_mut()?.next.take();
+            head.as_mut()?.next = node;
+            node = head;
+            head = next;
+        }
+        node
     }
+}
+
+#[test]
+fn test() {
+    let a = Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 2, next: None })) }));
+    let b = Some(Box::new(ListNode { val: 2, next: Some(Box::new(ListNode { val: 1, next: None })) }));
+    assert_eq!(Solution::reverse_list(a), b);
 }
