2125867e7638acdc3dac31358f04dce3554207f1
ruislan <z17520@126.com>
2020-08-20 23:12:01 +0800

    solved offer27

--- src/offer/offer27.rs
+++ src/offer/offer27.rs
@@ -32,6 +32,21 @@ impl Solution {
         // 缩小范围就是
         // 1.子节点不为空的，就放入递归去处理
         // 最后翻转完了之后，返回头结点
+        // match root {
+        //     None => None,
+        //     Some(node) => {
+        //         let l = node.borrow_mut().left.clone();
+        //         let r = node.borrow_mut().right.clone();
+        //
+        //         node.borrow_mut().left = r.clone();
+        //         node.borrow_mut().right = l.clone();
+        //
+        //         Solution::mirror_tree(node.borrow_mut().left.clone());
+        //         Solution::mirror_tree(node.borrow_mut().right.clone());
+        //
+        //         Some(node)
+        //     }
+        // }
 
         // 方法2
         // 使用循环处理
@@ -40,7 +55,21 @@ impl Solution {
         // 1. 该节点的子节点都是空，不处理
         // 2. 该节点的子节点有一个不为空，就交换swap(left,right)
         // 将不为空的那个子节点放入stack
-        let mut root = root;
-        root
+        // Unsafe
+        // Passed 0ms 2.1mb
+        let (mut root, mut arr) = (root, Vec::new());
+        root.map(|root| unsafe {
+            arr.push(root.as_ptr());
+            while !arr.is_empty() {
+                let mut children = Vec::new();
+                for node in arr {
+                    std::mem::swap(&mut (*node).left, &mut (*node).right);
+                    if (*node).left.is_some() { children.push((*node).left.as_mut().unwrap().as_ptr()); }
+                    if (*node).right.is_some() { children.push((*node).right.as_mut().unwrap().as_ptr()); }
+                }
+                arr = children;
+            }
+            root
+        })
     }
 }
