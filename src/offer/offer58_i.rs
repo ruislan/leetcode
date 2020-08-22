bf35b5680e8ad399824e0b1555d01ea21e583065
ruislan <z17520@126.com>
2020-08-22 17:00:44 +0800

    solved offer58_i

--- src/offer/offer58_i.rs
+++ src/offer/offer58_i.rs
@@ -1,19 +1,44 @@
 use crate::offer::Solution;
+use std::borrow::BorrowMut;
 
 impl Solution {
     pub fn reverse_words(s: String) -> String {
         // 方法1
         // 利用库函数
         // 直接通过split whitespace分割字符串存储成vec
-        // 然后翻转vec之后，每个分割的term使用trim修剪
-        // 再组合成String返回
-
-        String::new()
+        // 然后翻转vec之后，再组合成String返回
+        // Passed 0ms 2.1mb
+        s.split_ascii_whitespace().rev().collect::<Vec<&str>>().join(" ")
 
         // 方法2
-        // 迭代s，因为单词是不可能由空格组成的，所以只要遇到空格就可以确定一个单词
-        // 将这个单词存储到vec中
-        // 翻转vec，然后除最后一个单词以外其他单词后面都添加一个空格
-        // 最后collect返回
+        // 一次成型，创建一个res为String和空格计数gap
+        // s.chars()，如果不是空格，空格计数大于0，就res.push(空格)和res.push(ch)，
+        // 并将空格计数计入空，如果是空格，就不Push，将空格计数加一gap += 1
+        // 返回res
+        // Passed 0ms 2.2mb
+        // let mut res = Vec::new();
+        // let mut str = String::new();
+        // for ch in s.chars() {
+        //     match ch {
+        //         ' ' => {
+        //             if !str.is_empty() { res.push(str); }
+        //             str = String::new();
+        //         }
+        //         _ => str.push(ch),
+        //     }
+        // }
+        // if !str.is_empty() { res.push(str); }
+        // res.reverse();
+        // res.join(" ")
     }
+}
+
+#[test]
+fn test() {
+    assert_eq!(Solution::reverse_words("".to_string()), "".to_string());
+    assert_eq!(Solution::reverse_words("a".to_string()), "a".to_string());
+    assert_eq!(Solution::reverse_words("  a some dt".to_string()), "dt some a".to_string());
+    assert_eq!(Solution::reverse_words("the sky is blue".to_string()), "blue is sky the".to_string());
+    assert_eq!(Solution::reverse_words("  hello world!  ".to_string()), "world! hello".to_string());
+    assert_eq!(Solution::reverse_words("a good   example".to_string()), "example good a".to_string());
 }
