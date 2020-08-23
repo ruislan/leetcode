72e5743a0ad0494155fc34d9663331544a14e029
ruislan <z17520@126.com>
2020-08-24 06:59:21 +0800

    solved offer58_ii

--- src/offer/offer58_ii.rs
+++ src/offer/offer58_ii.rs
@@ -3,9 +3,21 @@ use crate::offer::Solution;
 impl Solution {
     pub fn reverse_left_words(s: String, n: i32) -> String {
         // 方法1
-        // 将n与s.len()取余数
         // 然后取a = &[..n]和b = &[n..]
         // 然后将b放到前面加上a再返回String
-        String::new()
+        // let mut res = String::from(&s[n as usize..]);
+        // res.push_str(&s[..n as usize]);
+        // res
+        String::from(&s[n as usize..]) + &s[..n as usize]
     }
+}
+
+#[test]
+fn test() {
+    assert_eq!(Solution::reverse_left_words("".to_string(), 0), "".to_string());
+    assert_eq!(Solution::reverse_left_words("a".to_string(), 1), "a".to_string());
+    assert_eq!(Solution::reverse_left_words("ab".to_string(), 1), "ba".to_string());
+    assert_eq!(Solution::reverse_left_words("abcdefg".to_string(), 2), "cdefgab".to_string());
+    assert_eq!(Solution::reverse_left_words("abcdefg".to_string(), 6), "gabcdef".to_string());
+    assert_eq!(Solution::reverse_left_words("lrloseumgh".to_string(), 6), "umghlrlose".to_string());
 }
