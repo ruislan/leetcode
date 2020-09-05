1aaf9f6b7348d1527acbc408d2918e7a789c93a3
ruislan <z17520@126.com>
2020-09-05 10:30:06 +0800

    solved q1556

--- src/q/q1556.rs
+++ src/q/q1556.rs
@@ -3,12 +3,28 @@ use crate::q::Solution;
 impl Solution {
     pub fn thousand_separator(n: i32) -> String {
         // 方法1
-        // 创建一个计数器count，初始值为0，
-        // 创建一个字符数组来记录数字转换成字符和小数点的数组chars
-        // 循环，当n > 0 时，
-        // 如果counter == 3，chars.push('.'), count = 0,
-        // 当前数字num = n % 10， n /= 10， count += 1，chars.push(num)
-        // 最后chars倒叙并形成字符串
-        String::new()
+        // 直接将数字转换为字符串，创建一个空字符串res，创建一个处理统计count
+        // 将字符串倒序取出放入res，每隔3个(count%3==0)且不等于原字符串长度就加入一个点
+        // 最后res倒叙并形成字符串
+        let mut origin = n.to_string();
+        let (len, mut count, mut res) = (origin.len(), 0, String::new());
+        while let Some(ch) = origin.pop() {
+            count += 1;
+            res.push(ch);
+            if count % 3 == 0 && count != len { res.push('.'); }
+        }
+        res.chars().rev().collect()
     }
+}
+
+#[test]
+fn test() {
+    assert_eq!(Solution::thousand_separator(0), "0");
+    assert_eq!(Solution::thousand_separator(11), "11");
+    assert_eq!(Solution::thousand_separator(111), "111");
+    assert_eq!(Solution::thousand_separator(1234), "1.234");
+    assert_eq!(Solution::thousand_separator(60010), "60.010");
+    assert_eq!(Solution::thousand_separator(111111), "111.111");
+    assert_eq!(Solution::thousand_separator(1111111), "1.111.111");
+    assert_eq!(Solution::thousand_separator(i32::max_value()), "2.147.483.647");
 }
