d9eb067d5c49cf2bcaa75e338e1a949c30e8895d
ruislan <z17520@126.com>
2020-09-26 01:28:33 +0800

    solved q637

--- src/q/q637.rs
+++ src/q/q637.rs
@@ -24,6 +24,20 @@ impl Solution {
     pub fn average_of_levels(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<f64> {
         // 方法1
         // 广度优先，一层一层的计算平均值即可
-        Vec::new()
+        let (mut res, mut queue) = (Vec::new(), std::collections::VecDeque::new());
+        if root.is_some() { queue.push_back(root); }
+        while !queue.is_empty() {
+            let (mut sum, count) = (0_f64, queue.len() as f64);
+            for _ in 0..queue.len() {
+                let node = queue.pop_front().unwrap().unwrap();
+                let left = node.borrow_mut().left.take();
+                let right = node.borrow_mut().right.take();
+                if left.is_some() { queue.push_back(left); }
+                if right.is_some() { queue.push_back(right); }
+                sum += node.borrow().val as f64;
+            }
+            res.push(sum / count);
+        }
+        res
     }
 }
