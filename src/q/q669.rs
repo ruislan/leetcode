8a3ed44a88eb2e9417a20e514f25c4421c2af5a9
ruislan <ruislan@github.com>
2020-10-18 00:54:13 +0800

    solved q669

--- src/q/q669.rs
+++ src/q/q669.rs
@@ -1,3 +1,8 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use crate::q::Solution;
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
     pub val: i32,
@@ -16,10 +21,6 @@ impl TreeNode {
     }
 }
 
-use std::rc::Rc;
-use std::cell::RefCell;
-use crate::q::Solution;
-
 impl Solution {
     pub fn trim_bst(root: Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> Option<Rc<RefCell<TreeNode>>> {
         // 特点：BST，最小值，最大值
@@ -31,6 +32,29 @@ impl Solution {
         // 如果当前节点值小于low，表示此节点和此节点的左子节点剪枝，返回右子节点的递归结果
         // 如果当前节点值正好等于high，表示此节点的右子节点剪枝，此节点的左子节点等于左子节点递归后的结果，返回此节点
         // 如果当前节点值大于high，表示此节点和此节点的右子节点剪枝，返回左子节点的递归结果
-        root
+        // Passed 0ms 3.1mb
+        match root {
+            None => None,
+            Some(node) => {
+                let val = node.borrow().val;
+                let left = node.borrow_mut().left.take();
+                let right = node.borrow_mut().right.take();
+                if val > low && val < high {
+                    node.borrow_mut().left = Self::trim_bst(left, low, high);
+                    node.borrow_mut().right = Self::trim_bst(right, low, high);
+                    Some(node)
+                } else if val == low {
+                    node.borrow_mut().right = Self::trim_bst(right, low, high);
+                    Some(node)
+                } else if val < low {
+                    Self::trim_bst(right, low, high)
+                } else if val == high {
+                    node.borrow_mut().left = Self::trim_bst(left, low, high);
+                    Some(node)
+                } else {
+                    Self::trim_bst(left, low, high)
+                }
+            }
+        }
     }
 }
