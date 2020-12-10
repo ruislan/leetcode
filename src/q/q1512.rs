ece881f8b53b2d71f21fb879475752e62d43e157
ruislan <ruislan@github.com>
2020-12-10 10:58:04 +0800

    solved interview0503

--- src/q/q1512.rs
+++ src/q/q1512.rs
@@ -9,6 +9,7 @@ impl Solution {
         // 以nums[i]为索引，将里面的值加到count上，count += arr[nums[i]]
         // 然后将arr[nums[i]]增加1
         // 一直到迭代结束，返回count即是好数对的结果
+        // Passed 0ms 1.9mb
         let (mut count, mut arr) = (0, vec![0; 101]);
         nums.iter().for_each(|&x| {
             count += arr[x as usize];
