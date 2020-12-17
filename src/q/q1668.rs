564e5cfcee079c4501575075aa0a3c45731e1fab
ruislan <ruislan@github.com>
2020-12-17 14:21:29 +0800

    solved q1668

--- src/q/q1668.rs
+++ src/q/q1668.rs
@@ -4,11 +4,21 @@ use crate::q::Solution;
 impl Solution {
     pub fn max_repeating(sequence: String, word: String) -> i32 {
         // 方法1
-        // n = word.len()
-        // 首先找到word第一个匹配的位置i，
-        // 然后比对i + len.. i + 2 * len - 1的位置是否相等
-        // 相等了之后继续向前移动，直到结束位置 > sequence.len()
-        // 返回记数结果
-        0
+        // 令s = word
+        // 如果sequence包含s，那么就将word加入到s中，重复判断，直到不包含为止
+        // 返回包含的统计次数
+        // Passed 0ms 2mb
+        let mut answer = 0;
+        let mut s = word.clone();
+        while sequence.contains(&s) {
+            s.push_str(&word);
+            answer += 1;
+        }
+        answer
     }
+}
+
+#[test]
+fn test() {
+    assert_eq!(Solution::max_repeating("ababc".to_string(), "ab".to_string()), 2);
 }
