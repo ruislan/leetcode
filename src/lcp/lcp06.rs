d0170efe00bfd9a4fb10e21e6068a9dc04e356f9
ruislan <ruislan@github.com>
2021-01-04 09:26:56 +0800

    solved lcp07

--- src/lcp/lcp06.rs
+++ src/lcp/lcp06.rs
@@ -5,6 +5,7 @@ impl Solution {
     pub fn min_count(coins: Vec<i32>) -> i32 {
         // 方法1
         // 迭代数组coins，然后coins[i] / 2 加上 coins[i] % 2，最后sum这些值得到结果
+        // Passed 0ms 1.9mb
         coins.iter().map(|coin| coin / 2 + coin % 2).sum()
     }
 }
