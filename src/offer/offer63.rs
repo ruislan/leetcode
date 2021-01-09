4f8e263a313722777b80af1a7e19dcad47aad19a
ruislan <ruislan@github.com>
2021-01-10 00:41:32 +0800

    solved offer63

--- src/offer/offer63.rs
+++ src/offer/offer63.rs
@@ -17,6 +17,7 @@ impl Solution {
         // 而我们还可以简化，不用记录所有的cols的最大的利润
         // 因为，我们只需要那个最大的利润，所以我们只留下最大的那个利润就行了
         // Passed 0ms 2.2mb
+        if prices.is_empty() { return 0; }
         let mut max_profit = 0;
         let mut min = prices[0];
         for col in 1..prices.len() {
