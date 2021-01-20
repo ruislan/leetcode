6940da1f98845f9d805a366e4ca13aaa75de0c2c
ruislan <ruislan@github.com>
2021-01-20 10:35:35 +0800

    solved xh_1_1

--- src/xh/mod.rs
+++ src/xh/mod.rs
@@ -1,3 +1,4 @@
 pub struct Solution;
 
+mod xh_1_1;
 mod xh_1_2;
---
+++ src/xh/xh_1_1.rs
@@ -0,0 +1,20 @@
+use crate::xh::Solution;
+
+impl Solution {
+    pub fn find_left_repeat_num(nums: Vec<i32>) -> Vec<i32> {
+        // 方法1
+        // 用hashmap记录第一次出现的位置就行了
+        // Passed 0ms 2.1mb
+        let mut hashmap = std::collections::HashMap::new();
+        let mut answer = Vec::new();
+        for i in 0..nums.len() {
+            if let Some(&first_pos) = hashmap.get(&nums[i]) {
+                answer.push(first_pos as i32);
+            } else {
+                hashmap.insert(nums[i], i);
+                answer.push(-1);
+            }
+        }
+        answer
+    }
+}
