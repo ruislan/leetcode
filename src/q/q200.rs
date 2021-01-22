5c84f6fc74b9c84a389c307e7230ef079f526923
ruislan <ruislan@github.com>
2021-01-22 09:43:30 +0800

    solved q200

--- src/q/q200.rs
+++ src/q/q200.rs
@@ -7,52 +7,59 @@ impl Solution {
         // 并查集
         // 将grid[row][col] == '1'的四周的联通的'1'的放入并查集
         // 然后统计并查集的集合数量就是结果
-        // fn find(mut x: usize, parents: &mut Vec<usize>) -> usize {
-        //     if x != parents[x] {
-        //         parents[x] = find(parents[x], parents);
+        // Passed 8ms 5.4mb
+        // fn find(mut x: usize, parent: &mut Vec<usize>) -> usize {
+        //     if x != parent[x] {
+        //         parent[x] = find(parent[x], parent);
         //     }
-        //     parents[x]
+        //     parent[x]
         // }
         //
-        // fn union(mut x: usize, mut y: usize, parents: &mut Vec<usize>) -> bool {
-        //     let mut root_x = find(x, parents);
-        //     let mut root_y = find(y, parents);
+        // fn union(mut x: usize, mut y: usize, parent: &mut Vec<usize>) -> bool {
+        //     let mut root_x = find(x, parent);
+        //     let mut root_y = find(y, parent);
         //     if root_x == root_y { return false; }
-        //     parents[root_y] = root_x;
+        //     parent[root_y] = root_x;
         //     true
         // }
         //
-        // fn index_of(row:usize, col:usize, cols:usize) -> usize { row * cols + col }
+        // fn index_of(row: usize, col: usize, cols: usize) -> usize { row * cols + col }
         //
         // let rows = grid.len();
         // let cols = grid[0].len();
         // let n = rows * cols;
-        // let mut parents = vec![0; n as usize];
-        // for i in 0..n as usize { parents[i] = i; }
+        // let mut parent = vec![0; n as usize];
+        // for i in 0..n as usize { parent[i] = i; }
         //
-        // let mut avaliables = std::collections::HashSet::new();
+        // let mut land = std::collections::HashSet::new();
         // for row in 0..rows {
         //     for col in 0..cols {
         //         if grid[row][col] == '1' {
-        //             avaliables.insert(index_of(row, col, cols));
+        //             land.insert(index_of(row, col, cols));
         //             if row < rows - 1 && grid[row + 1][col] == '1' {
-        //                 union(index_of(row, col, cols), index_of(row + 1, col, cols), &mut parents);
+        //                 union(index_of(row, col, cols), index_of(row + 1, col, cols), &mut parent);
         //             }
         //             if col < cols - 1 && grid[row][col + 1] == '1' {
-        //                 union(index_of(row, col, cols), index_of(row, col + 1, cols), &mut parents);
+        //                 union(index_of(row, col, cols), index_of(row, col + 1, cols), &mut parent);
+        //             }
+        //             if row > 0 && grid[row - 1][col] == '1' {
+        //                 union(index_of(row, col, cols), index_of(row - 1, col, cols), &mut parent);
+        //             }
+        //             if col > 0 && grid[row][col - 1] == '1' {
+        //                 union(index_of(row, col, cols), index_of(row, col - 1, cols), &mut parent);
         //             }
         //         }
         //     }
         // }
         //
-        // let mut set = std::collections::HashSet::new();
-        // for parent in parents {
-        //     if avaliables.contains(&parent) {
-        //         set.insert(parent);
+        // let mut island = std::collections::HashSet::new();
+        // for i in 0..n {
+        //     if land.contains(&i) {
+        //         island.insert(find(i, &mut parent));
         //     }
         // }
         //
-        // set.len() as i32
+        // island.len() as i32
 
         // 方法2
         // DFS，其实很容易想到DFS，
@@ -60,27 +67,59 @@ impl Solution {
         // 然后一个visited的存储已经浏览过的'1'，
         // 这样，每次走完一次DFS，就是一个集合（岛屿）被遍历
         // 统计结果就行了
+        // Passed 8ms 5.2mb
+        // let rows = grid.len();
+        // let cols = grid[0].len();
+        // let mut answer = 0;
+        // let mut visited = std::collections::HashSet::new();
+        // for row in 0..rows {
+        //     for col in 0..cols {
+        //         if grid[row][col] == '0' || visited.contains(&(row, col)) {
+        //             continue;
+        //         }
+        //         answer += 1;
+        //         let mut stack = vec![(row, col)];
+        //         while !stack.is_empty() {
+        //             let (row, col) = stack.pop().unwrap();
+        //             if visited.insert((row, col)) {
+        //                 if row < rows - 1 && grid[row + 1][col] == '1' {
+        //                     stack.push((row + 1, col));
+        //                 }
+        //                 if col < cols - 1 && grid[row][col + 1] == '1' {
+        //                     stack.push((row, col + 1));
+        //                 }
+        //                 if row > 0 && grid[row - 1][col] == '1' {
+        //                     stack.push((row - 1, col));
+        //                 }
+        //                 if col > 0 && grid[row][col - 1] == '1' {
+        //                     stack.push((row, col - 1));
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // answer
+
+        // 方法3
+        // 可以让访问过的“1”变成“0”，这样可以节约很多搜索和hash运算
+        // rust的hash很昂贵
+        // Passed 8ms 5.3mb
         let rows = grid.len();
         let cols = grid[0].len();
         let mut answer = 0;
-        let mut visited = std::collections::HashSet::new();
+        let mut grid = grid;
         for row in 0..rows {
             for col in 0..cols {
-                if grid[row][col] == '0' || visited.contains(&(row, col)) {
-                    continue;
-                }
+                if grid[row][col] == '0' { continue; }
                 answer += 1;
                 let mut stack = vec![(row, col)];
                 while !stack.is_empty() {
                     let (row, col) = stack.pop().unwrap();
-                    if visited.insert((row, col)) {
-                        if row < rows - 1 && grid[row + 1][col] == '1' {
-                            stack.push((row + 1, col));
-                        }
-                        if col < cols - 1 && grid[row][col + 1] == '1' {
-                            stack.push((row, col + 1));
-                        }
-                    }
+                    grid[row][col] = '0';
+                    if row < rows - 1 && grid[row + 1][col] == '1' { stack.push((row + 1, col)); }
+                    if col < cols - 1 && grid[row][col + 1] == '1' { stack.push((row, col + 1)); }
+                    if row > 0 && grid[row - 1][col] == '1' { stack.push((row - 1, col)); }
+                    if col > 0 && grid[row][col - 1] == '1' { stack.push((row, col - 1)); }
                 }
             }
         }
--- src/q/q989.rs
+++ src/q/q989.rs
@@ -3,17 +3,45 @@ use crate::q::Solution;
 #[allow(unused)]
 impl Solution {
     pub fn add_to_array_form(mut a: Vec<i32>, mut k: i32) -> Vec<i32> {
-        let mut carry = 0i32;
-        let mut res = Vec::new();
-        loop {
-            if a.is_empty() && k == 0 && carry == 0 { break; }
-            if let Some(n) = a.pop() { carry += n; }
-            carry += k % 10;
-            res.push(carry % 10);
+        // 方法1
+        // Passed 12ms 2mb
+        // let mut carry = 0i32;
+        // let mut answer = Vec::new();
+        // loop {
+        //     if a.is_empty() && k == 0 && carry == 0 { break; }
+        //     if let Some(n) = a.pop() { carry += n; }
+        //     carry += k % 10;
+        //     answer.push(carry % 10);
+        //     k /= 10;
+        //     carry /= 10;
+        // }
+        // answer.reverse();
+        // answer
+
+        // 方法1的另外一种写法
+        let mut k = k;
+        let mut b = Vec::new();
+        while k > 0 {
+            b.push(k % 10);
             k /= 10;
-            carry /= 10;
         }
-        res.reverse();
-        res
+
+        let mut a = a;
+        let mut carry = 0;
+        let mut answer = Vec::new();
+        let (mut i, mut j) = (0, 0);
+        a.reverse();
+        while i < a.len() || j < b.len() {
+            let x = if i < a.len() { a[i] } else { 0 };
+            let y = if j < b.len() { b[i] } else { 0 };
+            let sum = x + y + carry;
+            carry = sum / 10;
+            answer.push(sum % 10);
+            i += 1;
+            j += 1;
+        }
+        if carry > 0 { answer.push(carry); }
+        answer.reverse();
+        answer
     }
 }
