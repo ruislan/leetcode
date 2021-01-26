4eb47a1a5509d0c57984179f1a7a8e22bc2fa969
ruislan <ruislan@github.com>
2021-01-26 14:37:08 +0800

    solved q1732

--- src/q/mod.rs
+++ src/q/mod.rs
@@ -449,5 +449,6 @@ mod q1711;
 mod q1716;
 mod q1720;
 mod q1722;
+mod q1732;
 
 pub struct Solution;
---
+++ src/q/q1732.rs
@@ -0,0 +1,18 @@
+use crate::q::Solution;
+
+#[allow(unused)]
+impl Solution {
+    pub fn largest_altitude(gain: Vec<i32>) -> i32 {
+        // 方法1
+        // gain存储的是逐步的高度差
+        // 那么就依次加上g就是当前这个点的海拔高度
+        // 然后找出最大的那个就行了
+        let mut altitude = 0;
+        let mut answer = 0;
+        for g in gain {
+            altitude += g;
+            answer = answer.max(altitude);
+        }
+        answer
+    }
+}
