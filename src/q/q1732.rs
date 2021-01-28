e55aa15275fdeda457e09654d9242672bb595643
ruislan <ruislan@github.com>
2021-01-28 08:51:23 +0800

    solved q1732

--- src/q/q1732.rs
+++ src/q/q1732.rs
@@ -7,6 +7,7 @@ impl Solution {
         // gain存储的是逐步的高度差
         // 那么就依次加上g就是当前这个点的海拔高度
         // 然后找出最大的那个就行了
+        // AC 0ms 2mb
         let mut altitude = 0;
         let mut answer = 0;
         for g in gain {
--- src/q/q724.rs
+++ src/q/q724.rs
@@ -3,6 +3,11 @@ use crate::q::Solution;
 #[allow(unused)]
 impl Solution {
     pub fn pivot_index(nums: Vec<i32>) -> i32 {
+        // 方法1
+        // 分别求出左到右和右到左的前缀和
+        // 前缀和相等之处就是答案
+        // 否则就没有答案
+        // AC 0ms 2.2mb
         let mut sl = 0;
         let mut sr = 0;
         for i in (0..nums.len()).rev() {
