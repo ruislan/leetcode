b0ce8062940237fc597c5b4566f7029d61f4cf2a
ruislan <ruislan@github.com>
2021-01-29 11:19:42 +0800

    solved q386

--- src/q/q386.rs
+++ src/q/q386.rs
@@ -12,6 +12,7 @@ impl Solution {
         // 当然回溯一般都是用到递归的
         // 这里需要注意的是，递归的离开条件就是当前的数字
         // 大于了n，那么就不在寻找
+        // AC 12ms 2.6mb
         fn gen(mut x: i32, n: i32, arr: &mut Vec<i32>) {
             if x <= n {
                 arr.push(x);
@@ -26,5 +27,15 @@ impl Solution {
             gen(i, n, &mut answer);
         }
         answer
+
+        // 方法2
+        // 可以将所有的数字转换成字符串
+        // 然后直接排序，就是字典排序
+        // 然后再转换回数字即可
+        // 这个方法略微有点讨巧，应该不是本题考试目的
+        // AC 20~36ms 4.7~4.9mb
+        // let mut arr: Vec<String> = (1..=n).map(|x| x.to_string()).collect();
+        // arr.sort_unstable();
+        // arr.into_iter().map(|x| x.parse::<i32>().unwrap()).collect()
     }
 }
