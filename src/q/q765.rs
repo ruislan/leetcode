0e057d4129662672ccd765e669b4177d2e0c221c
ruislan <ruislan@github.com>
2021-02-05 09:52:41 +0800

    solved q765

--- src/q/q765.rs
+++ src/q/q765.rs
@@ -11,16 +11,19 @@ impl Solution {
         // 与你下一个数字交换，如果找不到，那么说明你下一个数字就是和你一对的
         // 然后跳过2个数字，重复直到数组结束
         // 感觉成了简单题了，所以这题可能考察的不是这样的解题方式，这就是出题出失败了呀
+        // 有时间就思考一下其他解法吧
+        // AC 0ms 1.9mb
         let n = row.len();
         let mut row = row;
         let mut i = 0;
         let mut answer = 0;
         while i < n - 2 {
-            let to_find = if row[i] % 2 == 0 { row[i] + 1 } else { row[i] - 1 };
+            let mate = if row[i] % 2 == 0 { row[i] + 1 } else { row[i] - 1 };
             for j in i + 2..n {
-                if row[j] == to_find {
+                if row[j] == mate {
                     row.swap(j, i + 1);
                     answer += 1;
+                    break;
                 }
             }
             i += 2;
