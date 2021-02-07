96e5b535c067f4491b16b814c5a02465ba2a29db
ruislan <ruislan@github.com>
2021-02-07 10:56:24 +0800

    solved q665

--- src/q/q665.rs
+++ src/q/q665.rs
@@ -3,17 +3,35 @@ use crate::q::Solution;
 #[allow(unused)]
 impl Solution {
     pub fn check_possibility(nums: Vec<i32>) -> bool {
+        // 方法1
+        // 这题很经典
+        // 这里的中心思想就是要保持非递减！
+        // 当出现一次非递减情况时，也即是nums[i] > nums[i + 1]时
+        // 很容易想到的是，如果在非递减的下一个出现了递减，我们就让
+        // 下一个等于我自己也即是nums[i + 1] = nums[i]即可
+        // 但是这里有个问题，就是我自己[i]才是那个没有保持队形的人呢
+        // 例如 1 4 2 3，如果我们让2换成了4，那么就出现了问题
+        // 所以这里我们不能只看下一个，还要看上一个
+        // 也就是nums[i - 1]与nums[i + 1]比较，
+        //    如果nums[i + 1] >= nums[i - 1]：
+        //       那么我们就让nums[i] = nums[i + 1]
+        //    否则（也就是nums[i + 1] < nums[i - 1]）：
+        //       那么我们就让nums[i + 1] = nums[i]
+        // 看出来了吗？也就是始终把小的那个变成大的
+        // 这里还有一个情况就是，如果我是第0个，没有左边，那么只能向后一个取
+        // AC 0ms 1.9mb
+        let n = nums.len();
         let mut nums = nums;
-        let mut count = 0;
-        for i in 0..nums.len() - 1 {
+        let mut replaced = 0;
+        for i in 0..n - 1 {
             if nums[i] > nums[i + 1] {
                 if i == 0 || nums[i + 1] >= nums[i - 1] {
                     nums[i] = nums[i + 1];
                 } else {
                     nums[i + 1] = nums[i];
                 }
-                count += 1;
-                if count > 1 { return false; }
+                replaced += 1;
+                if replaced > 1 { return false; }
             }
         }
         true
--- src/q/q77.rs
+++ src/q/q77.rs
@@ -9,17 +9,39 @@ impl Solution {
         // 递归的出口就是组合的长度达到了k的长度
         // 每次递归就是将当前数字和它后面到n的所有数字进行组合（调用递归）
         // 这里为什么是当前数字和它后面到n的数字，因为1到当前数字已经在组合中了
-        fn gen(i: i32, n: i32, k: usize, prev: Vec<i32>, arr: &mut Vec<Vec<i32>>) {
-            if prev.len() == k { arr.push(prev); } else {
-                for j in i..=n {
-                    let mut cur = prev.clone();
-                    cur.push(j);
-                    gen(j + 1, n, k, cur, arr);
-                }
+        // AC 72ms 2.9mb
+        // fn gen(i: i32, n: i32, k: usize, prev: Vec<i32>, arr: &mut Vec<Vec<i32>>) {
+        //     if prev.len() == k {
+        //         arr.push(prev);
+        //     } else {
+        //         for j in i..=n {
+        //             let mut cur = prev.clone();
+        //             cur.push(j);
+        //             gen(j + 1, n, k, cur, arr);
+        //         }
+        //     }
+        // }
+        // let mut answer = Vec::new();
+        // gen(1, n, k as usize, Vec::new(), &mut answer);
+        // answer
+
+        // 方法2
+        // 不是很满意方法1的时间
+        // 我们来调整一下，在迭代的时候利用pop来少做一些vec的拷贝
+        // AC 12ms 2.6mb （嗯，果然少了很多少时间，毫秒必争！！）
+        fn gen(i: i32, n: i32, k: usize, arr: &mut Vec<i32>, answer: &mut Vec<Vec<i32>>) {
+            if arr.len() == k {
+                answer.push(arr.clone());
+                return;
+            }
+            for j in i..=n {
+                arr.push(j);
+                gen(j + 1, n, k, arr, answer);
+                arr.pop();
             }
         }
         let mut answer = Vec::new();
-        gen(1, n, k as usize, Vec::new(), &mut answer);
+        gen(1, n, k as usize, &mut Vec::new(), &mut answer);
         answer
     }
 }
