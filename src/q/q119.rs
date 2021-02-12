9e55d0c5e6abb646c744e7f203d3411dbb8c251f
ruislan <ruislan@github.com>
2021-02-12 08:26:52 +0800

    solved q718

--- src/q/q119.rs
+++ src/q/q119.rs
@@ -4,33 +4,36 @@ use crate::q::Solution;
 impl Solution {
     pub fn get_row(row_index: i32) -> Vec<i32> {
         // 方法1
-        // let mut rows:Vec<Vec<i32>> = Vec::new();
+        // AC 0ms 1.9mb
+        // let mut last_row: Vec<i32> = vec![1; 1];
         // let row_index = row_index as usize + 1;
-        // for i in 1..=row_index {
-        //     let mut row = vec![1;i];
+        // for i in 2..=row_index {
+        //     let mut row = vec![1; i];
         //     if i > 2 {
-        //         let last_row = rows.last().unwrap();
         //         for j in 1..i - 1 {
         //             row[j] = last_row[j] + last_row[j - 1];
         //         }
         //     }
-        //     if i == row_index { return row; }
-        //     else { rows.push(row); }
+        //     last_row = row;
         // }
-        // vec![]
-        
+        // last_row
+
         // 方法2
-        let mut last_row: Vec<i32> = vec![1; 1];
-        let row_index = row_index as usize + 1;
-        for i in 2..=row_index {
-            let mut row = vec![1; i];
-            if i > 2 {
-                for j in 1..i - 1 {
-                    row[j] = last_row[j] + last_row[j - 1];
-                }
-            }
-            last_row = row;
-        }
-        last_row
+        // 递归思想是一个很重要的思想
+        // 是很多算法的基础，是程序思维的开端之一
+        // 递归有两个要素，出口和处理
+        // 出口也叫基线，通常是最简单的情况，
+        //    本题就是当处于第0行的时候（从0开始数行）
+        // 处理就是将递归作为一个整体或者处于某个问题的中间过程来看待，
+        //    本题就是第N行，我们通过递归得到了N-1行，那么怎么计算第N行
+        //    当然是第一个数据为1，然后从1开始迭代N-1行的数据，
+        //    两两相加然后放入本行的结果，最后再添加1即可
+        // AC 0ms 2mb
+        if row_index == 0 { return vec![1]; }
+        let prev_row = Self::get_row(row_index - 1);
+        let mut row = vec![1];
+        (1..prev_row.len()).for_each(|i| row.push(prev_row[i - 1] + prev_row[i]));
+        row.push(1);
+        row
     }
 }
--- src/q/q718.rs
+++ src/q/q718.rs
@@ -6,14 +6,22 @@ impl Solution {
         // 方法1
         // 典型的动态规划
         // 这个题和两个字符串最长公共子串是一样的
+        // 复习这个题的时候，注意和最长公共子序列一起复习，
+        // 同时最长公共子序列最好能够推导出是哪些序列
+        // AC 60ms 5.9mb
         let mut answer = 0;
-        let mut dp = vec![vec![0; b.len()]; a.len()];
-        for i in 0..a.len() {
-            for j in 0..b.len() {
-                dp[i][j] = if i > 0 && j > 0 { dp[i - 1][j - 1] } else { 0 } + if a[i] == b[j] { 1 } else { 0 };
-                answer = answer.max(dp[i][j]);
+        let mut dp = vec![vec![0; b.len() + 1]; a.len() + 1];
+        for i in 1..=a.len() {
+            for j in 1..=b.len() {
+                if a[i - 1] == b[j - 1] {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    answer = answer.max(dp[i][j]);
+                }
             }
         }
         answer
+
+        // 方法2
+        // 利用滑动窗口比较重复子数组
     }
 }
