7c62d44680126b254781e9a4214ae635e968ae6f
ruislan <ruislan@github.com>
2021-02-14 00:19:00 +0800

    solved offer47

--- src/offer/offer47.rs
+++ src/offer/offer47.rs
@@ -7,6 +7,7 @@ impl Solution {
         // 典型的动态规划
         // dp[i][j]存储的是到当前这个位置最大的价值
         // 它是由上方和左方中的最大价值积累过来
+        // AC 0ms 2.4mb
         let mut grid = grid;
         let rows = grid.len();
         let cols = grid[0].len();
