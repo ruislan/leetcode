4ace31614edb98130acf22336a39b2ba3e2bf595
ruislan <ruislan@github.com>
2021-02-14 01:02:42 +0800

    add idea description to q1753

--- src/q/q1753.rs
+++ src/q/q1753.rs
@@ -3,6 +3,10 @@ use crate::q::Solution;
 #[allow(unused)]
 impl Solution {
     pub fn maximum_score(a: i32, b: i32, c: i32) -> i32 {
+        // 方法1
+        // 始终取最大的两个数来处理
+        // 直到出现2个0及以上
+        // 利用大顶堆来获取顶上两个最大的，处理完后再放入堆
         let mut answer = 0;
         let mut heap = std::collections::BinaryHeap::new();
         heap.push(a);
