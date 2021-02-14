9b2d14d4c93ba4078732c7caf7117998a2055130
ruislan <ruislan@github.com>
2021-02-15 00:32:12 +0800

    solved q1456

--- src/q/q1456.rs
+++ src/q/q1456.rs
@@ -7,6 +7,7 @@ impl Solution {
         // 这是一个固定了窗口长度为k的滑动窗口
         // 每次滑动注意进一个然后出一个
         // 进出看是不是元音，进就加1个，出就减1个
+        // AC 0ms 2.1mb
         let mut vowels = vec![false; 26];
         vowels[b'a' as usize - 97] = true;
         vowels[b'e' as usize - 97] = true;
