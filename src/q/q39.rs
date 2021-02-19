56f36585b526e07bda352e4196ea61f2b0fa34f3
ruislan <ruislan@github.com>
2021-02-19 12:19:53 +0800

    solved q39

--- src/q/q39.rs
+++ src/q/q39.rs
@@ -15,31 +15,55 @@ impl Solution {
         // 最后由于不能出现重复的组合，我们可以将每个结果排序，然后删除重复项，
         // Rust有dedup()方法，kotlin有distinct()方法，
         // 注意dedup()调用前要sort一下，Rust的dedup不是hashset的方式实现的
+        // AC 24ms 2.9mb
+        // fn combine(sum: i32, arr: Vec<i32>, candidates: &Vec<i32>, target: i32, answer: &mut Vec<Vec<i32>>) {
+        //     for &x in candidates {
+        //         let new_sum = sum + x;
+        //         if new_sum > target { break; }
+        //
+        //         let mut arr = arr.clone();
+        //         arr.push(x);
+        //         if new_sum == target {
+        //             answer.push(arr);
+        //         } else {
+        //             combine(new_sum, arr, candidates, target, answer);
+        //         }
+        //     }
+        // }
+        //
+        // let mut answer = Vec::new();
+        // let mut candidates = candidates;
+        // candidates.sort_unstable();
+        // combine(0, Vec::new(), &candidates, target, &mut answer);
+        // for v in answer.iter_mut() {
+        //     v.sort_unstable();
+        // }
+        // answer.sort_unstable();
+        // answer.dedup();
+        // answer
 
-        fn combine(sum: i32, arr: Vec<i32>, candidates: &Vec<i32>, target: i32, answer: &mut Vec<Vec<i32>>) {
-            for &x in candidates {
-                let new_sum = sum + x;
-                if new_sum > target { break; }
-
-                let mut arr = arr.clone();
-                arr.push(x);
-                if new_sum == target {
-                    answer.push(arr);
-                } else {
-                    combine(new_sum, arr, candidates, target, answer);
-                }
+        // 方法2
+        // 我不是很满意方法1的时间，因为sort和clone太多了，
+        // 我们用标准的回溯模板来试试
+        // AC 0ms 2.2mb
+        fn combine(arr: &mut Vec<i32>, candidates: &Vec<i32>, begin: usize, target: i32, answer: &mut Vec<Vec<i32>>) {
+            if target == 0 {
+                answer.push(arr.clone());
+                return;
+            }
+            if target < 0 {
+                return;
+            }
+            for i in begin..candidates.len() {
+                let new_target = target - candidates[i];
+                arr.push(candidates[i]);
+                combine(arr, candidates, i, new_target, answer);
+                arr.pop();
             }
         }
 
         let mut answer = Vec::new();
-        let mut candidates = candidates;
-        candidates.sort_unstable();
-        combine(0, Vec::new(), &candidates, target, &mut answer);
-        for v in answer.iter_mut() {
-            v.sort_unstable();
-        }
-        answer.sort_unstable();
-        answer.dedup();
+        combine(&mut Vec::new(), &candidates, 0, target, &mut answer);
         answer
     }
 }
