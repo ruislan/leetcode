fd4ca3c1299b49ba62d74ed696a1fb2724223118
ruislan <ruislan@github.com>
2021-03-13 03:35:33 +0800

    solved q78

--- src/q/q78.rs
+++ src/q/q78.rs
@@ -8,7 +8,8 @@ impl Solution {
         // 这里由于每个情况都是结果，所以每个情况都要保存一下
         // 然后就是退出条件就是选择列表没有可以再有可选的即可
         // 由于输入的数据互不相同，所以我们只需要先排序即可。
-        fn backtrace(arr:&mut Vec<i32>, nums: &Vec<i32>, i:usize, answer:&mut Vec<Vec<i32>>) {
+        // AC 0ms 2.2mb
+        fn backtrace(arr: &mut Vec<i32>, nums: &Vec<i32>, i: usize, answer: &mut Vec<Vec<i32>>) {
             answer.push(arr.clone());
             for j in i..nums.len() {
                 arr.push(nums[j]);
