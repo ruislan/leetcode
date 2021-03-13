201df26649c2614e144cf0f6fd193de290f77518
ruislan <ruislan@github.com>
2021-03-14 00:20:15 +0800

    solved q90

--- src/q/q90.rs
+++ src/q/q90.rs
@@ -9,8 +9,9 @@ impl Solution {
         // 然后就是退出条件就是选择列表没有可以再有可选的即可
         // 由于输入的数据有重复的，所以产生的结果有可能重复，
         // 简单点直接采用hashset来解决就行了
+        // AC 0ms 2.1mb
         use std::collections::HashSet;
-        fn backtrace(arr:&mut Vec<i32>, nums: &Vec<i32>, i:usize, answer:&mut HashSet<Vec<i32>>) {
+        fn backtrace(arr: &mut Vec<i32>, nums: &Vec<i32>, i: usize, answer: &mut HashSet<Vec<i32>>) {
             answer.insert(arr.clone());
             for j in i..nums.len() {
                 arr.push(nums[j]);
