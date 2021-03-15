f40a14a649b3595f70655e9dc3a6cf1148f6a865
ruislan <ruislan@github.com>
2021-03-15 09:39:40 +0800

    solved q1641

--- src/q/q1641.rs
+++ src/q/q1641.rs
@@ -8,19 +8,27 @@ impl Solution {
         // 套用回溯模板就行了
         // 不用管元音，就看成5个数字就行
         // 这个题如果数学和概率学很熟悉的话，应该会有更简单的数学方法解决
-        fn backtrace(path: &mut Vec<i32>, n: usize, i: i32, answer: &mut i32) {
-            if path.len() == n {
-                *answer += 1;
-                return;
-            }
-            for x in i..5 {
-                path.push(x);
-                backtrace(path, n, x, answer);
-                path.pop();
-            }
-        }
-        let mut answer = 0;
-        backtrace(&mut Vec::new(), n as usize, 0, &mut answer);
-        answer
+        // AC 116ms 1.9mb
+        // fn backtrace(path: &mut Vec<i32>, n: usize, i: i32, answer: &mut i32) {
+        //     if path.len() == n {
+        //         *answer += 1;
+        //         return;
+        //     }
+        //     for x in i..5 {
+        //         path.push(x);
+        //         backtrace(path, n, x, answer);
+        //         path.pop();
+        //     }
+        // }
+        // let mut answer = 0;
+        // backtrace(&mut Vec::new(), n as usize, 0, &mut answer);
+        // answer
+
+        // 方法2
+        // 数学方法
+        // C(4, n+4)
+        // (n+4)*..*(n+1) / (4*..*1)
+        // AC 0ms 2.1mb
+        (n + 4) * (n + 3) * (n + 2) * (n + 1) / 24
     }
 }
