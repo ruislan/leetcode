2feed0ba62332e145490b44180ecfff825512663
ruislan <ruislan@github.com>
2021-08-15 01:39:44 +0800

    solved q576

--- src/q/q576.rs
+++ src/q/q576.rs
@@ -13,23 +13,43 @@ impl Solution {
         // 我们将每个点(x,y)和已经走了的步数s做一个元组作为key来记忆这个位置的这个步数有多少种出界的可能
         // 这样就能极大的优化过程，快速求出结果
         // AC 8ms 2.9mb
+        // use std::collections::HashMap;
+        // fn dfs(x: i32, y: i32, s: i32, m: i32, n: i32, max_move: i32, mem: &mut HashMap<(i32, i32, i32), i32>) -> i32 {
+        //     if s > max_move { return 0; }
+        //     if x < 0 || y < 0 || x >= m || y >= n { return 1; }
+        //     if let Some(&v) = mem.get(&(x, y, s)) {
+        //         return v;
+        //     }
+        //     let mut ret = dfs(x + 1, y, s + 1, m, n, max_move, mem) as i64 +
+        //         dfs(x - 1, y, s + 1, m, n, max_move, mem) as i64 +
+        //         dfs(x, y + 1, s + 1, m, n, max_move, mem) as i64 +
+        //         dfs(x, y - 1, s + 1, m, n, max_move, mem) as i64;
+        //     ret = ret % 1000000007;
+        //     let ret = ret as i32;
+        //     mem.insert((x, y, s), ret);
+        //     ret
+        // }
+        //
+        // dfs(start_row, start_column, 0, m, n, max_move, &mut HashMap::new())
+
+        // 方法1.1
+        // 优化一下，也就是把mem换成三维数组
+        // AC 0ms 2.5mb
         use std::collections::HashMap;
-        fn dfs(x: i32, y: i32, s: i32, m: i32, n: i32, max_move: i32, mem: &mut HashMap<(i32, i32, i32), i32>) -> i32 {
+        fn dfs(x: i32, y: i32, s: i32, m: i32, n: i32, max_move: i32, mem: &mut Vec<Vec<Vec<i32>>>) -> i32 {
             if s > max_move { return 0; }
             if x < 0 || y < 0 || x >= m || y >= n { return 1; }
-            if let Some(&v) = mem.get(&(x, y, s)) {
-                return v;
-            }
+            if mem[x as usize][y as usize][s as usize] != -1 { return mem[x as usize][y as usize][s as usize]; }
             let mut ret = dfs(x + 1, y, s + 1, m, n, max_move, mem) as i64 +
                 dfs(x - 1, y, s + 1, m, n, max_move, mem) as i64 +
                 dfs(x, y + 1, s + 1, m, n, max_move, mem) as i64 +
                 dfs(x, y - 1, s + 1, m, n, max_move, mem) as i64;
             ret = ret % 1000000007;
             let ret = ret as i32;
-            mem.insert((x, y, s), ret);
+            mem[x as usize][y as usize][s as usize] = ret;
             ret
         }
 
-        dfs(start_row, start_column, 0, m, n, max_move, &mut HashMap::new())
+        dfs(start_row, start_column, 0, m, n, max_move, &mut vec![vec![vec![-1; max_move as usize + 1]; n as usize]; m as usize])
     }
 }
