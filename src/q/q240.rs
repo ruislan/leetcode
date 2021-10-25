595d426a581ed8f76d9267f68621bf6e058b9e7b
ruislan <ruislan@github.com>
2021-10-25 14:42:42 +0800

    updated q240

--- src/q/q240.rs
+++ src/q/q240.rs
@@ -8,6 +8,7 @@ impl Solution {
         // 方法1
         // 直接迭代matrix，因为有序，内部vec采用二分查找，存在target即返回true
         // 迭代完返回false
+        // AC 4ms 2.5mb 129/129
         // matrix.iter().find(|&x| x.binary_search(&target).is_ok()).is_some()
 
         // 方法2
@@ -18,10 +19,15 @@ impl Solution {
         // 循环上述三行，直到row = matrix.len() - 1，column = 0（最后一行，第一列）
         // 都没有找到，说明数字不在其中，返回false
         // 注意rust中column为0时减去1会溢出成最大的那个数字，所以判断条件是column < columns而不是column >= 0
+        // AC 4ms 2.7mb
         let (rows, columns) = (matrix.len(), if matrix.is_empty() { 0 } else { matrix[0].len() });
         let (mut row, mut column) = (0, columns - 1);
         while row < rows && column < columns {
-            if matrix[row][column] < target { row += 1; } else if matrix[row][column] > target { column -= 1; } else { return true; }
+            match matrix[row][column].cmp(&target) {
+                std::cmp::Ordering::Greater => column -= 1,
+                std::cmp::Ordering::Less => row += 1,
+                std::cmp::Ordering::Equal => return true,
+            }
         }
         false
     }
