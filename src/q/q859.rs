af958bd613ba954654174f18218df4351b05af25
ruislan <ruislan@github.com>
2021-11-23 21:33:53 +0800

    updated q859

--- src/q/q859.rs
+++ src/q/q859.rs
@@ -2,54 +2,34 @@ use crate::q::Solution;
 
 #[allow(unused)]
 impl Solution {
-    // 给定两个由小写字母构成的字符串 A 和 B ，只要我们可以通过交换 A 中的两个字母得到与 B 相等的结果，就返回 true ；否则返回 false 。
-    // 0 <= A.length <= 20000
-    // 0 <= B.length <= 20000
-    // A 和 B 仅由小写字母构成。
-    pub fn buddy_strings(a: String, b: String) -> bool {
-        if a.len() != b.len() {
-            return false;
+    pub fn buddy_strings(s: String, goal: String) -> bool {
+        // 方法1
+        // 交换一次的情况下要相等
+        // 1. 那么首先要保证的就是两个字符串的长度要相等
+        // 2. 然后两个字符串的字符频率要一样
+        // 3. 接着检查两个字符串的对位不相同的字符数量是否为2或者没有
+        //   3.1 如果为2，那一定可以交换
+        //   3.2 如果没有，那么就要判断是否至少有一个字符的频率要大于等于2（至少2个才可以交换）
+        // AC 0ms 2.2mb 34/34
+        let s: Vec<char> = s.chars().collect();
+        let goal: Vec<char> = goal.chars().collect();
+        if s.len() != goal.len() { return false; }
+        let mut diff = 0;
+        let mut freq_s = vec![0; 26];
+        let mut freq_goal = vec![0; 26];
+        for i in 0..s.len() {
+            if s[i] != goal[i] { diff += 1; }
+            freq_s[(s[i] as u8 - b'a') as usize] += 1;
+            freq_goal[(goal[i] as u8 - b'a') as usize] += 1;
         }
-        let mut buckets = vec![0; 256];
-        let a = a.chars().collect::<Vec<char>>();
-        let b = b.chars().collect::<Vec<char>>();
-        let mut swap = (0, 0);
-        let mut count = 0;
-        for i in 0..a.len() {
-            let ch1 = a[i] as u8;
-            let ch2 = b[i] as u8;
-            buckets[ch1 as usize] += 1;
-
-            if ch1 != ch2 {
-                if count > 2 {
-                    return false;
-                }
-                if count == 0 {
-                    swap.0 = ch1;
-                    swap.1 = ch2;
-                } else if swap.0 != ch2 || swap.1 != ch1 {
-                    return false;
-                }
-                count += 1;
+        for i in 0..26 {
+            if freq_s[i] != freq_goal[i] { return false; }
+        }
+        if diff == 0 {
+            for i in 0..26 {
+                if freq_s[i] > 1 { return true; }
             }
         }
-        if count == 0 {
-            for i in 0..256 {
-                if buckets[i] > 1 {
-                    return true;
-                }
-            }
-        }
-        count != 0
+        diff == 2
     }
-}
-
-#[test]
-fn test_q859() {
-    assert_eq!(true, Solution::buddy_strings("ba".to_string(), "ab".to_string()));
-    assert_eq!(false, Solution::buddy_strings("ab".to_string(), "ab".to_string()));
-    assert_eq!(false, Solution::buddy_strings("abc".to_string(), "abc".to_string()));
-    assert_eq!(false, Solution::buddy_strings("abcd".to_string(), "abcd".to_string()));
-    assert_eq!(true, Solution::buddy_strings("aba".to_string(), "aba".to_string()));
-    assert_eq!(true, Solution::buddy_strings("aa".to_string(), "aa".to_string()));
 }
